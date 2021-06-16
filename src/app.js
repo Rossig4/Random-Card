3e14de687e846aac94e066aca3d1f5e06baff6b2
Rossig4 <r.silvera.ag@gmail.com>
2021-06-16 21:39:34 +0000

    listo para entregar

--- src/app.js
+++ src/app.js
@@ -2,4 +2,44 @@
 import "bootstrap";
 import "./style.css";
 
-//write your code here
+let generateCard = () => {
+  let character = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  return character[Math.floor(Math.random() * character.length)];
+};
+
+let generateSymbol = () => {
+  let figure = ["corazones", "diamantes", "pica", "trebol"];
+  let figureIndex = Math.floor(Math.random() * figure.length);
+  return figure[figureIndex];
+};
+
+let symbol = generateSymbol();
+let number = generateCard();
+
+document.querySelector(".btn").addEventListener("click", () => {
+  document.querySelector(".card").classList.remove(`${symbol}`);
+  symbol = generateSymbol();
+  number = generateCard();
+
+  document.querySelector(".card").classList.add(`${symbol}`);
+  document.querySelector(".card").innerHTML = `${number}`;
+});
+
+window.onload = () => {
+  document.querySelector(".card").classList.add(symbol);
+  document.querySelector(".card").innerHTML = number;
+};
